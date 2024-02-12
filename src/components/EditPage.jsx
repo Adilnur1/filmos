3e1321b330ac208b8eb35debb398a0a275511b22
Aidar <ajydarbakytov@gmail.com>
2024-02-12 15:11:01 +0600

    aidar

--- src/components/EditPage.jsx
+++ src/components/EditPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const EditPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Adiar</h1>
+      <p></p>
+    </div>
+  );
 };
 
 export default EditPage;
